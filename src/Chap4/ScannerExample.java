2495d98a697e298d8966804b419132a038f006e0
papercut <papercut@172.17.60.230>
2019-08-13 09:13:30 +0700

    1 commit

---
+++ src/Chap4/ScannerExample.java
@@ -0,0 +1,52 @@
+package Chap4;
+
+import java.util.Scanner;
+
+public class ScannerExample {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter name : ");
+        String name =sc.next();
+        System.out.print("name is "+name);
+        
+        System.out.print("Enter age :");
+        int age=sc.nextInt(); 
+        System.out.print("Please enter your salary:");
+        double salary = sc.nextDouble();
+        System.out.print("Employee name :"+name);
+        System.out.println(",age : "+age+",salary :"+salary);
+        
+        if(age<1|| age>65){
+            System.out.println("Error!!GPA must be 1-65");
+        }else if(age<11){
+            System.out.println("วัยเด็ก");
+        }else if(age>=20){
+            System.out.println("วัยรุ่น");
+        }else if(age>=40){
+            System.out.println("วัยผู้ใหญ่");
+        }else if(age>=60){
+            System.out.println("วัยกลางคน");
+        }else if(age>=65){
+            System.out.println("วัยสูงอายุ");
+        }
+        System.out.println("Enter your salary : ");
+            System.out.println("Enter your degree : ");
+            int degree = sc.nextInt ();
+            switch (degree){
+            case 1:
+                System.out.println("ปริญญาตรี");
+                break;
+            case 2:
+                System.out.println("ปริญญาโท");
+                break;
+            case 3:
+                System.out.println("ปริญญาเอก");
+                break;
+            default:
+                System.out.println("ไม่ระบุ");
+                break;
+        }
+        System.out.println("Employee name :"+name);
+        System.out.println(",age : "+age+",salary : "+salary);   
+    }
+}
